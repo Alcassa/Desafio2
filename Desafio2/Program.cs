2fd5f4a4b9f88f567d96fafda50c9bd76f5c7858
Alcassa <galcassa49@gmail.com>
2023-10-18 15:32:04 -0300

    Add project files.

---
+++ Desafio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Desafio2.models;
+
+namespace Desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nokia = new Nokia("213213", "mod1", "839imei", 64);
+            Console.WriteLine("Smartphone Nokia: ");
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Telegram");
+
+            Console.WriteLine();
+
+            var iphone = new Iphone("8457", "mod2", "imei", 512);
+            Console.WriteLine("Smartphone Iphone: ");
+            iphone.ReceberLigacao();
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Whatsapp");
+        }
+    }
+}
---
+++ Desafio2/models/Smartphone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2.models
+{
+    abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; 
+        }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public Smartphone(string numero,string modelo,string imei,int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+        public abstract void InstalarAplicativo(string nomeApp);
+
+    }
+}
